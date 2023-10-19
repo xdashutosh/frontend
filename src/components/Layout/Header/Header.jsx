0366570da98a4988ec15011a78e9beffd379d1c0
ashuchaudhary6969@gmail.com <ashuchaudhary6969@gmail.com>
2023-10-19 17:13:03 +0530

    navigation

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Drawer,
     DrawerBody,
-    DrawerFooter,
     DrawerHeader,
     DrawerOverlay,
     DrawerContent,
     DrawerCloseButton,
     Button,
-    Input,
     useDisclosure,
     HStack,
+    VStack,
+    DrawerFooter,
   } from '@chakra-ui/react'
   import {BiMenuAltLeft} from 'react-icons/bi'
   import {ColorModeSwitcher} from '../../../ColorModeSwitcher'
+import { Link } from 'react-router-dom'
+import { RiDashboardFill, RiLogoutBoxLine} from 'react-icons/ri'
+
+const LinkBut =({url="/",title="Home"})=>(
+<Link to={url}>
+    <Button variant={'ghost'} >{title}</Button>
+</Link>
+);
+
+
 const Header = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const btnRef = React.useRef()
+
+
+const [isAuthenticated ,setisAuthenticated] = useState(false);
+const user = {
+    role:"admin"
+};
+
   return (
    
     <HStack w={'full'} justifyContent={'space-between'}>
@@ -34,19 +51,59 @@ const Header = () => {
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
-          <DrawerHeader>𝕃earn <b>𝕍ita</b></DrawerHeader>
+          <DrawerHeader borderBottomWidth={'1px'}>𝕃earn <b>𝕍ita</b></DrawerHeader>
 
           <DrawerBody>
-          
+          <VStack spacing={'8'} alignItems={'flex-start'}>
+            <LinkBut url={'/'} title={'Home'} />
+            <LinkBut url={'/courses'} title={'Browse All Courses'}/>
+            <LinkBut url={'/request'} title={'Request a Course'}/>
+            <LinkBut url={'/contact'} title={'Contact Us'}/>
+            <LinkBut url={'/about'} title={'About'}/>
+           
+          </VStack>
+           
+           
           </DrawerBody>
 
           <DrawerFooter>
-            <Button variant='outline' mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme='blue'>Save</Button>
-          </DrawerFooter>
+        {isAuthenticated?(    
+                <VStack>
+
+                <HStack mt={'16'} spacing={'8'}>
+                   <Link to={'/login'}>
+                            <Button colorScheme='green' variant={'ghost'} >Profile</Button>
+                           </Link>
+                            <Link to={'/'}>
+                            <Button  onClick={()=>setisAuthenticated(false)}><RiLogoutBoxLine/> Logout</Button>
+                           </Link>
+                    
+                </HStack>
+                
+                <HStack >
+{
+    user && user.role==="admin" && <Link to={'/admin/dashboard'}>
+    <Button colorScheme='green'><RiDashboardFill/>dashboard</Button>
+    </Link>
+}
+                           </HStack>       
+                </VStack>
+                           ):(
+                            <HStack spacing={'4'} mt={'16'}>
+                                <Link to={'/login'}>
+                            <Button colorScheme='green' onClick={()=>setisAuthenticated(true)}>Login</Button>
+                           </Link>
+                <text>or</text>
+                <Link to={'/signup'}>
+                <Button variant={'solid'}>Signup</Button>
+                </Link>
+                </HStack>
+                            )}
+
+        </DrawerFooter>
+
         </DrawerContent>
+       
       </Drawer>
       <ColorModeSwitcher/>
           </HStack>
@@ -54,4 +111,5 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
+
