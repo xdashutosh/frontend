af8b0d4e85f26d567a514c4484aa7e5371d1e8b3
ashuchaudhary6969@gmail.com <ashuchaudhary6969@gmail.com>
2023-10-20 19:38:41 +0530

    contactus

---
+++ src/components/Contactus/Contact.jsx
@@ -0,0 +1,17 @@
+import { Container, Heading, VStack } from '@chakra-ui/react'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <Container h={'90vh'}>
+<VStack>
+    <Heading children={'Contact Us'}/>
+    
+</VStack>
+      </Container>
+    </div>
+  )
+}
+
+export default Contact
