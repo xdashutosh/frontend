8404790ea9c353995ec62a54fefad2ac904c6b5d
ashuchaudhary6969@gmail.com <ashuchaudhary6969@gmail.com>
2023-10-24 20:21:58 +0530

    graph

--- src/components/Admin/Dashboard.jsx
+++ src/components/Admin/Dashboard.jsx
@@ -1,93 +1,158 @@
-import { Box, Grid, HStack, Heading, Progress, Stack, Text } from '@chakra-ui/react'
-import React from 'react'
-import cursor from '../../assets/images/Cursor.png'
-import Sidebar from './Sidebar'
-import {RiArrowUpLine,RiArrowDownLine} from "react-icons/ri"
+import {
+  Box,
+  Grid,
+  HStack,
+  Heading,
+  Progress,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import React from 'react';
+import cursor from '../../assets/images/Cursor.png';
+import Sidebar from './Sidebar';
+import { RiArrowUpLine, RiArrowDownLine } from 'react-icons/ri';
+import { DoubnutChart, LineChart } from '../Chart';
 
-
-const Databox = ({title,qty,qtypercent,profit})=>{
-  return(
-    <Box w={['full','40%','20%']} boxShadow={'lg'} p={'8'} borderRadius={'lg'}>
-<Text fontWeight={'bold'}>{title}</Text>
-<HStack>
-  <HStack spacing={'8'}>
-  <Text fontSize={'2xl'} fontWeight={'bolder'}>{qty}</Text>
-  <Text>{`${qtypercent}%`}</Text>
-  </HStack>
-  {profit?<RiArrowUpLine color='green'/>:<RiArrowDownLine color='red'/>}
-</HStack>
-<Text fontSize={'sm'} opacity={'0.5'}>Since last month</Text>
+const Databox = ({ title, qty, qtypercent, profit }) => {
+  return (
+    <Box
+      w={['full', '40%', '20%']}
+      boxShadow={'lg'}
+      p={'8'}
+      borderRadius={'lg'}
+    >
+      <Text fontWeight={'bold'}>{title}</Text>
+      <HStack>
+        <HStack spacing={'8'}>
+          <Text fontSize={'2xl'} fontWeight={'bolder'}>
+            {qty}
+          </Text>
+          <Text>{`${qtypercent}%`}</Text>
+        </HStack>
+        {profit ? (
+          <RiArrowUpLine color="green" />
+        ) : (
+          <RiArrowDownLine color="red" />
+        )}
+      </HStack>
+      <Text fontSize={'sm'} opacity={'0.5'}>
+        Since last month
+      </Text>
     </Box>
-  )
+  );
 };
 
-const Bar =({title,value,profit})=>{
-  return(
-<Box py={'4'} px={['0','20']}>
-<Heading size={'sm'} mb={'2'}>{title}</Heading>
-<HStack w={'full'} alignItems={'center'}>
-  <Text children={profit?"0%":`-${value}`}/>
-  <Progress w={'full'} value={value} colorScheme={profit?"blue":"red"}/>
-  <Text children="100%"/>
-</HStack>
-</Box>
-  )
-}
+const Bar = ({ title, value, profit }) => {
+  return (
+    <Box py={'4'} px={['0', '20']}>
+      <Heading size={'sm'} mb={'2'}>
+        {title}
+      </Heading>
+      <HStack w={'full'} alignItems={'center'}>
+        <Text children={profit ? '0%' : `-${value}`} />
+        <Progress
+          w={'full'}
+          value={value}
+          colorScheme={profit ? 'blue' : 'red'}
+        />
+        <Text children="100%" />
+      </HStack>
+    </Box>
+  );
+};
 
 const Dashboard = () => {
-
-
   return (
-    <Grid 
-    css={{
-      cursor: `url(${cursor}),default`,
-    }}
-    minH={'80vh'} templateColumns={['1fr','5fr 1fr']}
-    
+    <Grid
+      css={{
+        cursor: `url(${cursor}),default`,
+      }}
+      minH={'80vh'}
+      templateColumns={['1fr', '5fr 1fr']}
     >
-<Box 
-boxSizing='border-box'
-px={['4','0']}>
-<Text textAlign={'center'} opacity={'.5'}>Last update on {new Date().toISOString().split('T')[0]}</Text>
-<Heading children="DashBoard" textAlign={['center','start']} ml={['0','8']} borderBottomWidth={['0','2px']} w={['full','-webkit-max-content']}/>
-<Stack direction={['column','row']} justifyContent={'center'}>
-  <Databox title ="Views" qty={123} qtypercent={30} profit={true}/>
-  <Databox title ="Users" qty={32} qtypercent={78} profit={true}/>
-  <Databox title ="Subscription" qty={13} qtypercent={20} profit={false}/>
-</Stack>
-<Box
-mt={['4','16']}
-  boxShadow={'-5px 0px 10px darkGray'}
-  borderRadius={'lg'}
-  p={['4','8']}
-  mx={['0','16']}
-  >
-<Heading children="View Graph" fontSize={['xl','2xl']} textAlign={['center','start']} ml={['0','8']} borderBottomWidth={['0','2px']} w={['full','-webkit-max-content']}/>
-{/* Line graph */}
-  </Box>
-<Grid templateColumns={['1fr','2fr 1fr']}>
-<Box p={'4'}>
-<Heading textAlign={['center','left']} size={'md'} children="Progress Bar" my={'8'} ml={['0','16']}/>
-<Box>
-  <Bar title="views" value={30} profit={true}/>
-  <Bar title="Users" value={78} profit={true}/>
-  <Bar title="Subscription" value={20} profit={false}/>
-</Box>
-</Box>
+      <Box boxSizing="border-box" px={['4', '0']}>
+        <Text textAlign={'center'} opacity={'.5'}>
+          Last update on {new Date().toISOString().split('T')[0]}
+        </Text>
+        <Heading
+          children="DashBoard"
+          textAlign={['center', 'start']}
+          ml={['0', '8']}
+          borderBottomWidth={['0', '2px']}
+          w={['full', '-webkit-max-content']}
+        />
+        <Stack direction={['column', 'row']} justifyContent={'center'}>
+          <Databox title="Views" qty={123} qtypercent={30} profit={true} />
+          <Databox title="Users" qty={32} qtypercent={78} profit={true} />
+          <Databox
+            title="Subscription"
+            qty={13}
+            qtypercent={20}
+            profit={false}
+          />
+        </Stack>
+        <Box
+          mt={['4', '16']}
+          boxShadow={'-5px 0px 10px darkGray'}
+          borderRadius={'lg'}
+          p={['4', '8']}
+          mx={['0', '16']}
+        >
+          <Heading
+            children="View Graph"
+            fontSize={['xl', '2xl']}
+            textAlign={['center', 'start']}
+            ml={['0', '8']}
+            borderBottomWidth={['0', '2px']}
+            w={['full', '-webkit-max-content']}
+          />
+          {/* Line graph */}
+          <Box maxW={'100vw'}>
+          <LineChart/>
 
-<Box p={'4'}>
-<Heading textAlign={['center','left']} size={'md'} children="Pie chart" my={'8'} ml={['0','16']}/>
-<Box>
- 
-</Box>
-</Box>
+          </Box>
+        </Box>
+      
 
-</Grid>
+        <Grid templateColumns={['1fr', '2fr 1fr']}>
+          <Box p={'4'}>
+            <Heading
+              textAlign={['center', 'left']}
+              size={'md'}
+              children="Progress Bar"
+              my={'8'}
+              ml={['0', '16']}
+              />
+         
 
-</Box>
-<Sidebar/>
+            <Box mt={['0','24']}> 
+              <Bar title="views" value={30} profit={true} />
+              <Bar title="Users" value={78} profit={true} />
+              <Bar title="Subscription" value={20} profit={false} />
+            </Box>
+        
+          </Box>
+
+          <Box p={'4'}>
+            <Heading
+              textAlign={['center', 'left']}
+              size={'md'}
+              children="Pie chart"
+              my={'8'}
+              ml={['0', '16']}
+              />
+            <Box >
+
+            <DoubnutChart/>
+
+            </Box>
+          </Box>
+        </Grid>
+            
+      </Box>
+      <Sidebar />
     </Grid>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
---
+++ src/components/Chart.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import {Chart as ChartJS,CategoryScale,LinearScale,Tooltip,PointElement,LineElement,Title,ArcElement,Legend} from 'chart.js';
+import {Line,Doughnut}  from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,LinearScale,Tooltip,PointElement,LineElement,Title,ArcElement,Legend
+)
+export const LineChart = () => {
+
+    const labels =["abhi","ashu","arjit"];
+    const options ={
+        responsive:true,
+        plugins:{
+            legend:{
+             position:"bottom"
+            },
+            title:{
+                display:true,
+                text:"Yearly views"
+            }
+        }
+    }
+
+const data ={
+    labels,
+    datasets:[
+        {
+            label:"Views",
+            data:[1,2,3],
+            borderColor:"rgba(107,70,193,0.5)",
+            backgroundColor:"#6b4c1"
+
+        }
+    ]
+};
+  return (
+   <Line options={options} data={data} />
+  )
+}
+
+export const DoubnutChart = () =>{
+
+
+   
+const data ={
+    labels:["subscribed","not subscribed"],
+    datasets:[
+        {
+            label:"Views",
+            data:[3,20],
+            borderColor:["blue","red"],
+            backgroundColor:["blue","red"],
+            borderWidth:1
+
+        }
+    ]
+};
+
+return(
+    <>
+    <Doughnut  data={data}/>
+    </>
+)
+};
+function getLastyearlebels(){
+    const labels=[];
+    const months =[
+        "Jan","Feb","Mar","Apr","May","June","Jul","Aug","Sep","Oct","Nov","Dec"
+    ];
+
+    const currentmonth = new Date().getMonth();
+    const remain = 11-currentmonth;
+    console.log(remain);
+    for (let index = 0; index < remain; index++) {
+        labels.push(months[index]);
+        
+    }
+    console.log(labels);
+}
+getLastyearlebels();
